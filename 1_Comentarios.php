710481e7d685af077b8f253f0c7dcd5926593079
StephanieOrtizPico <nicolleortiz@colegioguanenta.edu.co>
2023-07-17 17:26:54 -0500

    intriducciones_PHP

---
+++ 1_Comentarios.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Comentarios en php</title>
+</head>
+<body>
+    <h1>Comentario en PHP</h1>
+    <!..Formas de incluir codigo php..>
+
+
+    <?php
+        //comentarios de una linea
+        /*Esto es un comentario de 
+        varias lineas */
+    ?>
+
+    <!..
+    <%
+        codigo php
+    %>
+
+
+    <script language="php">
+
+    </script>..>
+</body>
+</html>
